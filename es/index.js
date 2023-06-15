08a90a127a2711d536724c83fcf635ce3a6d2acd
JunaidTariq <junaid.tariq48@gmail.com>
2023-06-16 00:08:10 +0400

    updated eslint

--- es/index.js
+++ es/index.js
@@ -1,33 +1,15 @@
 import thunk from 'redux-thunk';
 
-function createRealThunks(input, output, dispatch, getState) {
-  const getProps =
-    arguments.length > 4 && arguments[4] !== undefined
-      ? arguments[4]
-      : function() {};
-
+function createRealThunks(input, output, dispatch, getState, getProps = () => {}) {
   const actions = {};
 
   Object.keys(output.actions).forEach(actionKey => {
-    actions[actionKey] = function() {
-      let _output$actions;
-
-      return dispatch(
-        (_output$actions = output.actions)[actionKey].apply(
-          _output$actions,
-          arguments
-        )
-      );
-    };
+    actions[actionKey] = (...actionArgs) => dispatch(output.actions[actionKey](...actionArgs));
     actions[actionKey].toString = output.actions[actionKey].toString;
   });
 
-  const get = function get(key) {
-    return key
-      ? output.selectors[key](getState(), getProps())
-      : output.selector(getState(), getProps());
-  };
-  const fetch = function fetch() {
+  const get = key => key ? output.selectors[key](getState(), getProps()) : output.selector(getState(), getProps());
+  const fetch = function () {
     const results = {};
 
     const keys = Array.isArray(arguments[0]) ? arguments[0] : arguments;
@@ -39,67 +21,31 @@ function createRealThunks(input, output, dispatch, getState) {
     return results;
   };
 
-  return input.thunks(
-    Object.assign({}, output, {
-      actions,
-      dispatch,
-      getState,
-      get,
-      fetch
-    })
-  );
+  return input.thunks(Object.assign({}, output, { actions, dispatch, getState, get, fetch }));
 }
 
-const _injectToClass = function _injectToClass(Klass, input, output) {
+const injectToClass = (Klass, input, output) => {
   if (Klass.prototype._injectedKeaThunk) {
-    console.error(
-      `[KEA] Error! Already injected kea thunk into component "${(Klass &&
-        Klass.name) ||
-        Klass}"`
-    );
+    console.error(`[KEA] Error! Already injected kea thunk into component "${Klass && Klass.name || Klass}"`);
   }
   Klass.prototype._injectedKeaThunk = true;
 
   const originalComponentWillMount = Klass.prototype.componentWillMount;
-  Klass.prototype.componentWillMount = function() {
-    const _this = this;
-
+  Klass.prototype.componentWillMount = function () {
     // this === component instance
-    let realThunks = void 0;
+    let realThunks;
     const thunkKeys = Object.keys(input.thunks(output));
     const thunkFunctions = {};
 
     thunkKeys.forEach(thunkKey => {
-      thunkFunctions[thunkKey] = function() {
-        for (
-          var _len = arguments.length, args = Array(_len), _key = 0;
-          _key < _len;
-          _key++
-        ) {
-          args[_key] = arguments[_key];
+      thunkFunctions[thunkKey] = (...args) => (dispatch, getState) => {
+        if (!realThunks) {
+          realThunks = createRealThunks(input, output, dispatch, getState, () => this.props);
         }
-
-        return function(dispatch, getState) {
-          let _realThunks;
-
-          if (!realThunks) {
-            realThunks = createRealThunks(
-              input,
-              output,
-              dispatch,
-              getState,
-              () => _this.props
-            );
-          }
-          return (_realThunks = realThunks)[thunkKey].apply(_realThunks, args);
-        };
+        return realThunks[thunkKey](...args);
       };
     });
-    output.created.actions = Object.assign(
-      {},
-      output.created.actions,
-      thunkFunctions
-    );
+    output.created.actions = Object.assign({}, output.created.actions, thunkFunctions);
     output.actions = Object.assign({}, output.actions, thunkFunctions);
 
     originalComponentWillMount && originalComponentWillMount.bind(this)();
@@ -113,66 +59,41 @@ export default {
   global: true,
   local: false,
 
-  beforeReduxStore: function beforeReduxStore(options) {
+  beforeReduxStore: options => {
     options.middleware.push(thunk);
   },
 
-  isActive: function isActive(input) {
-    return !!input.thunks;
-  },
+  isActive: input => !!input.thunks,
 
-  afterCreateSingleton: function afterCreateSingleton(input, output) {
+  afterCreateSingleton: (input, output) => {
     if (output.activePlugins.thunk) {
-      let realThunks = void 0;
+      let realThunks;
       const thunkKeys = Object.keys(input.thunks(output));
       const thunkFunctions = {};
 
       thunkKeys.forEach(thunkKey => {
-        thunkFunctions[thunkKey] = function() {
-          for (
-            var _len2 = arguments.length, args = Array(_len2), _key2 = 0;
-            _key2 < _len2;
-            _key2++
-          ) {
-            args[_key2] = arguments[_key2];
+        thunkFunctions[thunkKey] = (...args) => (dispatch, getState) => {
+          if (!realThunks) {
+            realThunks = createRealThunks(input, output, dispatch, getState);
           }
-
-          return function(dispatch, getState) {
-            let _realThunks2;
-
-            if (!realThunks) {
-              realThunks = createRealThunks(input, output, dispatch, getState);
-            }
-            return (_realThunks2 = realThunks)[thunkKey].apply(
-              _realThunks2,
-              args
-            );
-          };
+          return realThunks[thunkKey](...args);
         };
       });
 
-      output.created.actions = Object.assign(
-        {},
-        output.created.actions,
-        thunkFunctions
-      );
+      output.created.actions = Object.assign({}, output.created.actions, thunkFunctions);
       output.actions = Object.assign({}, output.actions, thunkFunctions);
     }
   },
 
-  injectToClass: function injectToClass(input, output, Klass) {
+  injectToClass: (input, output, Klass) => {
     if (output.activePlugins.thunk) {
-      _injectToClass(Klass, input, output);
+      injectToClass(Klass, input, output);
     }
   },
 
-  injectToConnectedClass: function injectToConnectedClass(
-    input,
-    output,
-    KonnektedKlass
-  ) {
+  injectToConnectedClass: (input, output, KonnektedKlass) => {
     if (output.activePlugins.thunk) {
-      _injectToClass(KonnektedKlass, input, output);
+      injectToClass(KonnektedKlass, input, output);
     }
   }
   // injectToConnectedClass
